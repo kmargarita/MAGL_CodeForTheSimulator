3213f5ec041fe14fd88dbdfca3aafdec11ffde65
kmargarita <margaritawelbourne@gmail.com>
2021-01-04 03:36:33 +0000

    Main.c

--- mms-c-master/Main.c
+++ mms-c-master/Main.c
@@ -98,16 +98,141 @@ void updateWalls(int x, int y, int o, int L, int R, int F){
 	    cells[y][x]=6;
 	}
 	else if(o==3){
-	    cells[y][x]=8;
+	    cells[y][x]=5;
 	}
     }
     else if(R && F && !L){
         if(o==0){
 	    cells[y][x]=7;
 	}
+	else if(o==1){
+	    cells[y][x]=6;
+	}
+	else if(o==2){
+	    cells[y][x]=5;
+	}
+	else if(o==3){
+	    cells[y][x]=8;
+	}
+    }
+    else if(F){
+        if(o==0){
+	    cells[y][x]=2;
+	}
+	else if(o==1){
+	    cells[y][x]=3;
+	}
+	else if(o==2){
+	    cells[y][x]=4;
+	}
+	else if(o==3){
+	    cells[y][x]=1;
+	}
+    }
+    else if(L){
+        if(o==0){
+	    cells[y][x]=1;
+	}
+	else if(o==1){
+	    cells[y][x]=2;
+	}
+	else if(o==2){
+	    cells[y][x]=3;
+	}
+	else if(o==3){
+	    cells[y][x]=3;
+	}
+    }
+    else if(R){
+        if(o==0){
+	    cells[y][x]=3;
+	}
+	else if(o==1){
+	    cells[y][x]=4;
+	}
+	else if(o==2){
+	    cells[y][x]=1;
+	}
+	else if(o==3){
+	    cells[y][x]=2;
+	}
     }
 
 }
+
+bool isAccessible(int x, int y, int x1, int y1){
+    // Returns True if mouse can move to (x1,y1) from(x,y)
+    if(x==x1){
+	if(y>y1){
+	    if(cells[y][x]== 4 || cells[y][x]== 5 || cells[y][x]== 6 || cells[y][x]== 10 || cells[y][x]== 11 || cells[y][x]== 12 || cells[y][x]== 14){
+	        //4,5,6,10,11,12,14.
+	        return(false);
+	    }
+	    else{
+	        return(true);
+	    }
+
+    	}
+	else{
+	    if(cells[y][x]== 2 || cells[y][x]== 7 || cells[y][x]== 8 || cells[y][x]== 10 || cells[y][x]== 12 || cells[y][x]== 13 || cells[y][x]== 14){
+	        //2,7,8,10,12,13,14
+		return(false);
+	    }
+	    else{
+	        return(true);
+	    }
+	}
+    }
+
+    else if(y==y1){
+        //
+        if(x>x1){
+            if(cells[y][x]==1 || cells[y][x]==5 || cells[y][x]==8 || cells[y][x]==9 || cells[y][x]==11 || cells[y][x]==13 || cells[y][x]==14){
+                return (false);
+	    }
+            else{
+                return (true);
+	    }
+	}
+	else{
+            if(cells[y][x]==3 || cells[y][x]==6 || cells[y][x]==7 || cells[y][x]==9 || cells[y][x]==11 || cells[y][x]==12 || cells[y][x]==13){
+                return (false);
+	    }
+            else{
+                return (true);
+	    }
+	}
+    }
+
+}
+
+int getSurrounds(int x, int y){
+    // Return x1, y1,x2,y2, x3, y3, x4, y4 (surrounding square)
+    int x3=x-1;
+    int y3=y;
+    int x0=x;
+    int y0=y+1;
+    int x1=x+1;
+    int y1=y;
+    int x2=x;
+    int y2=y-1;
+    if(x1>=16){
+        x1=-1;
+    }
+    if(y0>=16){
+        y0=-1;
+    }
+    return(x0,y0,x1,y1,x2,y2,x3,y3); // north,east,south,west.
+}
+
+bool isConsistant(int x, int y){
+    // Returns True if value of current square is one 
+    // greater than the miniumum value in an accessible neighbour
+    x0,y0,x1,y1,x2,y2,x3,y3 = getSurrounds(x,y); // N,E,S,W
+    
+}
+
+
 
 void logTxt(char* text) {
     fprintf(stderr, "%s\n", text);
