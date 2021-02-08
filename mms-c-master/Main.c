59cb2962f43bb55b556249a7382877fff009bcf3
kmargarita <margaritawelbourne@gmail.com>
2021-02-08 22:56:41 +0000

    all

--- mms-c-master/Main.c
+++ mms-c-master/Main.c
@@ -62,45 +62,30 @@ struct number_hash{
     UT_hash_handle hh; /* Makes structure hashable*/
 };
 
-void destroy_table(struct number_hash** table){
-    struct number_hash* curr;
-    struct number_hash* tmp;
-    HASH_ITER(hh, *table, curr, tmp){
-        HASH_DEL(*table, curr);
-	free(curr);
-    }
-}
 
-void append(int value){
 
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    struct number_hash *table = NULL;
-    struct number_hash *element;
-    int *ret =  (int*) malloc(2 * sizeof(int));
-    int remaining;
-    for (int i=0; i<numsSize; i++){
-        remaining = target - nums[i];
-        
-        // Add new number to the hash table if not already added
-        HASH_FIND_INT(table, &remaining, element);
-        if(element){
-            ret[0] = element->index;
-            ret[1] = i;
-            break;
-        }
 
-        HASH_FIND_INT(table, &nums[i], element);
-        if(!element){
-            element = (struct number_hash *) malloc(sizeof(*element));
-            element->value = nums[i];
-            element->index = i;
-            HASH_ADD_INT(table, value, element);
-    }
-    }
-    destroy_table(&table);
-    *returnSize = 2;
-    return ret;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 Updating The Walls: (cells[y][x]=)
